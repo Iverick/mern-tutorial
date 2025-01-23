ee325a332f0a7b21e108135636a60deee723c80e
Pavel <iverick091@gmail.com>
2025-01-23 20:32:55 +0000

    Prepared for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -8,13 +10,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
-});
-
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
